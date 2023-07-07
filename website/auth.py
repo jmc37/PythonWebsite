dce6b7d046105f2aa7c6fb0fab0dc7e49d50e7c3
Joseph Chan <josephchan36@gmail.com>
2023-07-06 20:19:13 -0700

    added creating and deleting notes

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 auth = Blueprint('auth', __name__)
 
 
@@ -15,16 +17,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in succesfully', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again', category='error')
         else:
             flash("User email doesn't exist", category='error')
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -51,6 +57,7 @@ def sign_up():
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account created.', category='success')
             return redirect(url_for('views.home'))
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
